ffc967de1d862eb186811749d3824f3b3274d140
leesangheon <bleach8625@gmail.com>
2023-03-28 18:36:08 +0900

    초기 셋팅

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-practice';
+  title = 'angular-practice123da1123312312331dasd23';
+  title1 = 'angular-practice';
+  title2 = 'angular-practice1';
+  title3 = 'angular-practice';
 }
