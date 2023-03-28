0460c29a7fc4205e6103ca97fcf347424cc5a6b7
leesangheon <bleach8625@gmail.com>
2023-03-28 21:04:39 +0900

    lazy-loading 작업

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'step1',
+        loadChildren: () =>
+            import('./app-main/app-main.module').then((m) => m.AppMainModule),
+    },
+    {
+        path: '',
+        redirectTo: '',
+        pathMatch: 'full',
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-practice123da1123312312331dasd23';
-  title1 = 'angular-practice';
-  title2 = 'angular-practice1';
-  title3 = 'angular-practice';
-}
+export class AppComponent {}
